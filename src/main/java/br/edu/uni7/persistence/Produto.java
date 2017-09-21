c0b266ea976b86ca7fa39e8c8938e806306de09c
Notebook <janine.freitas.ti@gmail.com>
2017-09-20 21:54:04 -0300

    "teste commit"

--- src/main/java/br/edu/uni7/persistence/Produto.java
+++ src/main/java/br/edu/uni7/persistence/Produto.java
@@ -13,8 +13,6 @@ import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
-import org.hibernate.annotations.ManyToAny;
-
 @Entity
 @Table(name = "TBL_PRODUTOS")
 public class Produto {
